d8db6cddfbc57b1f4603c381e0b54157f4cc56cc
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-05-21 12:58:48 +0200

    called on all the innerhtml elements

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
+
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  }
