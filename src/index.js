8b14d375d201ba1bbc7c835a0040be66ded64413
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-05-21 13:02:05 +0200

    calling the search function using the apikey ad apiurl

--- src/index.js
+++ src/index.js
@@ -37,3 +37,20 @@ function refreshWeather(response) {
   
     return `${day} ${hours}:${minutes}`;
   }
+  function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+  
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Paris");
