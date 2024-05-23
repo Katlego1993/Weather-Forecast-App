b918b0de9ba628e6eaf48c7ecba1002328c26766
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-05-23 17:49:10 +0200

    insected the URL Key

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "d55f84f83a43423t2ocb40270f1aa5ba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value)
 }
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
